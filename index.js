4dbe11c75caf1b499431c128115f2b5b483d8c1b
harsilspatel <harsilspatel@gmail.com>
2019-05-13 11:51:27 +1000

    documented index.js

--- index.js
+++ index.js
@@ -8,14 +8,19 @@ const request = require('request');
 const mongoose = require('mongoose');
 const ArgumentParser = require('argparse').ArgumentParser;
 
+// initialising variables
 const args = getArgs();
 const CSV_URL = args.csv_url;
 const BATCH_SIZE = args.batch_size;
+
+// priority will be given if passing via console
 const MONGODB_URI = args.mongo_uri || process.env.MONGODB_URI;
 
+// importing models
 const Order = require('./models/Order');
 const Customer = require('./models/Customer');
 
+// connecting to mongoDB
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
@@ -23,23 +28,20 @@ db.once('open', () => {
   console.log('db connected');
   Customer.find({}, function (err, customers) {
     if (err) throw err;
+    // we get all the customers and create a set of their customerID
     streamCSV(new Set(customers.map(c => c.customerId)))
   });
 
 });
 
 function streamCSV(customerIdSet) {
-  console.log(BATCH_SIZE);
-  console.log(CSV_URL);
+  // variables for keeping count of entries processed
   var i = 0;
-  var j = 0;
   var counter = 0;
   var totalInserted = 0;
   var bulkOrder = Order.collection.initializeUnorderedBulkOp();
 
   const req = request.get(CSV_URL);
-
-
   req.on('error', function(err) {
     console.error(err);
   })
@@ -47,6 +49,8 @@ function streamCSV(customerIdSet) {
   csvStream
     .on('data', (order) => {
       console.log('processing line', ++i);
+
+      // we only insert the order if the customerId is in the database.
       if (customerIdSet.has(order.CountryName)) {
         bulkOrder.insert({
           orderId: order.CountryCode + i,
@@ -58,6 +62,8 @@ function streamCSV(customerIdSet) {
         counter++;
       }
 
+      // if 'BATCH_SIZE' number of elements are inserted in bulkOrder 
+      // then push them all to the database and update counters.
       if (counter == BATCH_SIZE) {
         csvStream.pause();
         console.log('PUSHINGGG!', counter)
@@ -71,6 +77,7 @@ function streamCSV(customerIdSet) {
       }
     })
     .on('end', function(){
+      // process any operations that are presernt in the bulkOrder
       if (counter) {
         console.log('PUSHINGGG!', counter)
         bulkOrder.execute(function(err, result) {
