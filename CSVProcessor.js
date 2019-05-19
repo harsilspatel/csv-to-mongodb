f16ba0062320950f4cefd577b130a5eda6ef05f6
harsilspatel <harsilspatel@gmail.com>
2019-05-20 02:48:19 +1000

    add and integrate csvprocessor

---
+++ CSVProcessor.js
@@ -0,0 +1,74 @@
+require('dotenv').config();
+const csv = require('fast-csv');
+const request = require('request');
+
+class CSVProcessor {
+  constructor(csvUrl, batchSize, resumeFrom) {
+    this.lineCount = 0;
+    this.csvUrl = csvUrl;
+    this.entriesInserted = 0;
+    this.batchSize = batchSize;
+    this.resumeFrom = resumeFrom;
+  }
+
+  async processCSV(controller, model, customerIdSet) {
+    // console.log(customerIdSet);
+    // process.exit()
+    // variables for keeping count of entries processed
+    let counter = 0; // to keep count of values in the bulk()
+    this.lineCount = 0; // to keep count of lines
+    this.entriesInserted = 0; // to keep track of total items inserted
+
+    const req = request.get(this.csvUrl);
+    req.on('error', err => {
+      console.error(err);
+    });
+    const csvStream = csv.fromStream(req, { headers: true });
+    csvStream
+      .on('data', order => {
+        this.lineCount++;
+        if (this.lineCount >= this.resumeFrom) {
+          console.log(`processing line # ${this.lineCount}`);
+
+          // we only insert the order if the customerId is in the database.
+          if (customerIdSet.has(order.customerId)) {
+            controller.insertIntoBulk(model, order);
+
+            // bulkOrder.insert(order);
+            counter++;
+            console.log(`items in bulk ${counter}`);
+          }
+
+          // if 'BATCH_SIZE' number of elements are inserted in bulkOrder
+          // then push them all to the database and update counters.
+          if (counter == this.batchSize) {
+            csvStream.pause();
+            console.log(`PUSHINGGG! ${counter} items`);
+            controller.executeBulk(model, (err, result) => {
+              if (err) throw err;
+              this.entriesInserted += counter;
+              counter = 0;
+              csvStream.resume();
+            });
+          }
+        }
+      })
+      .on('end', () => {
+        // process any operations that are presernt in the bulkOrder
+        if (counter) {
+          console.log(`PUSHINGGG! ${counter} items`);
+          controller.executeBulk(model, (err, result) => {
+            if (err) throw err;
+            this.entriesInserted += counter;
+            console.log('a total of', this.entriesInserted, 'entries inserted');
+            process.exit();
+          });
+        } else {
+          console.log('a total of', this.entriesInserted, 'entries inserted');
+          process.exit();
+        }
+      });
+  }
+}
+
+module.exports = CSVProcessor;
--- index.js
+++ index.js
@@ -6,123 +6,56 @@ const request = require('request');
 const mongoose = require('mongoose');
 const { ArgumentParser } = require('argparse');
 
-// importing models
+// importing helpers
 const Order = require('./models/Order');
 const Customer = require('./models/Customer');
+const CSVProcessor = require('./CSVProcessor');
 const DatabaseController = require('./DatabaseController');
 
 // initialising variables
 const args = getArgs();
-const CSV_URL = args.csv_url;
-const BATCH_SIZE = args.batch_size;
-const RESUME_FROM = args.resume_from;
+const processor = new CSVProcessor(
+  args.csv_url,
+  args.batch_size,
+  args.resume_from
+);
 
 // priority will be given if passing via console
 const MONGODB_URI = args.mongo_uri || process.env.MONGODB_URI;
 
-
-
-const controller = new DatabaseController(MONGODB_URI, BATCH_SIZE);
-controller.connect(() => console.error('failure'), () => {
-  controller.query(Customer, {}, (err, customers) => {
-    if (err) throw err;
-    // we get all the customers and create a set of their customerID
-    streamCSV(new Set(customers.map(c => c.customerId)));
-  });
-});
-
-
-
-// connecting to mongoDB
-// mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
-// const db = mongoose.connection;
-// db.on('error', console.error.bind(console, 'connection error:'));
-// db.once('open', () => {
-//   console.log('db connected');
-//   Customer.find({}, (err, customers) => {
-//     if (err) throw err;
-//     // we get all the customers and create a set of their customerID
-//     streamCSV(new Set(customers.map(c => c.customerId)));
-//   });
-// });
-
-async function streamCSV(customerIdSet) {
-  // variables for keeping count of entries processed
-  let i = 0; // to keep count of lines
-  let counter = 0; // to keep count of values in the bulk()
-  let totalInserted = 0; // to keep track of total items inserted
-  let bulkOrder = Order.collection.initializeUnorderedBulkOp();
-
-  const req = request.get(CSV_URL);
-  req.on('error', (err) => {
-    console.error(err);
-  });
-  const csvStream = csv.fromStream(req, { headers: true });
-  csvStream
-    .on('data', (order) => {
-      i++;
-      if (i >= RESUME_FROM) {
-        console.log('processing line', i);
-        // we only insert the order if the customerId is in the database.
-        if (customerIdSet.has(order.customerId)) {
-          controller.insertIntoBulk(Order, order);
-          // bulkOrder.insert(order);
-          counter++
-          console.log('add ', counter);
-        }
-
-        // if 'BATCH_SIZE' number of elements are inserted in bulkOrder
-        // then push them all to the database and update counters.
-        if (counter == BATCH_SIZE) {
-          csvStream.pause();
-          console.log('PUSHINGGG!', counter);
-          controller.executeBulk(Order, (err, result) => {
-            if (err) console.log(err);
-            totalInserted += counter;
-            counter = 0;
-            csvStream.resume();
-          })
-        }
-      }
-    })
-    .on('end', () => {
-      // process any operations that are presernt in the bulkOrder
-      if (counter) {
-        console.log('PUSHINGGG!', counter);
-        controller.executeBulk(Order, (err, result) => {
-          if (err) throw err;
-          totalInserted += counter;
-          console.log('a total of', totalInserted, 'entries inserted');
-          process.exit();
-        })
-      } else {
-        console.log('a total of', totalInserted, 'entries inserted');
-        process.exit();
-      }
+const controller = new DatabaseController(MONGODB_URI);
+controller.connect(
+  () => console.error('failure'),
+  () => {
+    controller.query(Customer, {}, (err, customers) => {
+      if (err) throw err;
+      // we get all the customers and create a set of their customerID
+      processor.processCSV(controller, Order, new Set(customers.map(c => c.customerId)));
     });
-}
+  }
+);
 
 function getArgs() {
   const parser = new ArgumentParser({
     version: '0.0.1',
     addHelp: true,
-    description: 'argparser for csv-to-mongodb app',
+    description: 'argparser for csv-to-mongodb app'
   });
   parser.addArgument(['-c', '--csv-url'], {
     required: true,
-    help: 'the url for the remote csv',
+    help: 'the url for the remote csv'
   });
   parser.addArgument(['-b', '--batch-size'], {
-    required: true,
-    help: 'batch size for inserting values in mongo database',
+    defaultValue: 1000,
+    help: 'batch size for inserting values in mongo database'
   });
   parser.addArgument(['-m', '--mongo-uri'], {
-    help: 'uri to connect to mongo database',
+    help: 'uri to connect to mongo database'
   });
   parser.addArgument(['-r', '--resume-from'], {
     defaultValue: 0,
     help:
-      'line number to resume the insertion from, in case the previous attempt was interrupted',
+      'line number to resume the insertion from, in case the previous attempt was interrupted'
   });
   return parser.parseArgs();
 }
