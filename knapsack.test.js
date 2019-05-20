9f6d76f17550e0ba47de9ecc5f272d52de84e54e
harsilspatel <harsilspatel@gmail.com>
2019-05-20 12:51:49 +1000

    complete logic for testing

--- knapsack.test.js
+++ knapsack.test.js
@@ -1,14 +1,13 @@
 const packAndReport = require('./knapsack');
 
 const testWeight = (weight, orders, byOId) =>
-  (orders.map(id => byOId[id]['weight']).reduce((t, a) => t + a, 0) <= weight);
+  orders.map(id => byOId[id]['weight']).reduce((t, a) => t + a, 0) <= weight;
 
-
-const byOrderId = (orders) => {
+const byOrderId = orders => {
   const orderObj = {};
   orders.forEach(o => (orderObj[o.orderId] = o));
-  return orderObj
-}
+  return orderObj;
+};
 
 function groupBy(array, fn) {
   const group = {};
@@ -22,16 +21,18 @@ function groupBy(array, fn) {
   return group;
 }
 
-const byCustomerId = (orders) => groupBy(orders, x => x.customerId);
+const byCustomerId = orders => groupBy(orders, x => x.customerId);
 
-const orderIdToVanId = (vans) => { //returns { def: 1, zzz: 1, ghi: 2, mno: 2, rst: 2, abc: 3 } for exampleOrders
-  const myObj = {}
+let setUnion = (a, b) => new Set([...a, ...b]);
+
+const orderIdToVanId = vans => {
+  //returns { def: 1, zzz: 1, ghi: 2, mno: 2, rst: 2, abc: 3 } for exampleOrders
+  const myObj = {};
   for (let i = 0; i < vans.length; i++) {
-    vans[i].orders.forEach(oId => myObj[oId] = i+1)
+    vans[i].orders.forEach(oId => (myObj[oId] = i + 1));
   }
   return myObj;
-}
-
+};
 
 const exampleOrders = [
   {
@@ -69,21 +70,31 @@ const exampleOrders = [
 function testKnapsack(orders, weight) {
   const result = packAndReport(orders, weight);
   groupByOrderId = byOrderId(orders);
-  const allPassing = result.vans.every(item => testWeight(weight, item.orders, groupByOrderId));
-  if (!(allPassing)) {
+  const allPassing = result.vans.every(item =>
+    testWeight(weight, item.orders, groupByOrderId)
+  );
+  if (!allPassing) {
     throw new Error('not all the orders abide by the weight constraints');
   }
 
   const orderIdToVanIdObj = orderIdToVanId(result.vans);
-  console.log(orderIdToVanIdObj);
 
-
-
-
-
+  const groupByCutomerId = groupBy(orders, x => x.customerId);
+  console.log(Object.values(groupByCutomerId));
+  const vans = Object.values(groupByCutomerId).map(
+    os => new Set(os.map(o => orderIdToVanIdObj[o.orderId]))
+  );
+  console.log(vans)
+  const spreadVans = vans.filter(s => s.size > 1).reduce(setUnion, new Set());
+  if (result.spreadVanIds.sort() == Array.from(spreadVans).sort()) {
+    throw new Error('the spread reported is incorrect')
+  } else {
+    console.log('works')
+  }
 }
 
+testKnapsack(exampleOrders, 5);
+testKnapsack([], 5);
 
-testKnapsack(exampleOrders, 5)
 
 // console.log(byOrderId(exampleOrders));
